3055a4406c5a2781a35c774cac787d2ee6741c6a
Aosanori <norihideoda@gmail.com>
2022-01-10 23:35:34 +0900

    ビームの情報を入力できるように

--- lib/beam_information/beam_information_display.dart
+++ lib/beam_information/beam_information_display.dart
@@ -10,6 +10,7 @@ class BeamInformationDisplay extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final beamInformationDisplayViewModel =
         ref.watch(beamInformationDisplayViewModelProvider);
+    final currentBeam = beamInformationDisplayViewModel.currentBeam;
     return Column(
       children: [
         const Text(
@@ -20,27 +21,89 @@ class BeamInformationDisplay extends HookConsumerWidget {
             height: 1.2,
           ),
         ),
-        DropdownButton<String>(
-          value: beamInformationDisplayViewModel.currentBeam.type,
-          icon: const Icon(Icons.arrow_drop_down),
-          iconSize: 30,
-          elevation: 16,
-          underline: Container(
-            height: 2,
-            color: Colors.grey,
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text(
+                    'Beam Type',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    'Wave Length',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    'Beam Waist',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DropdownButton<String>(
+                    value: beamInformationDisplayViewModel.currentBeam.type,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    iconSize: 30,
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                    onChanged: (newValue) {
+                      beamInformationDisplayViewModel.changeBeamType(newValue!);
+                    },
+                    items: beamTypes
+                        .map<DropdownMenuItem<String>>(
+                          (value) => DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                      controller: beamInformationDisplayViewModel.beamWaveLengthInputController,
+                      textAlign: TextAlign.end,
+                      decoration: const InputDecoration(
+                        suffixText: 'nm',
+                      ),
+                      onChanged:
+                          beamInformationDisplayViewModel.changeWaveLength,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                      controller: beamInformationDisplayViewModel.beamWaistInputController,
+                      textAlign: TextAlign.end,
+                      onChanged:
+                          beamInformationDisplayViewModel.changeBeamWaist,
+                      decoration: const InputDecoration(
+                        suffixText: 'mm',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          onChanged: (newValue) {
-            beamInformationDisplayViewModel.changeBeamType(newValue!);
-          },
-          items: beamTypes
-              .map<DropdownMenuItem<String>>(
-                (value) => DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                ),
-              )
-              .toList(),
-        )
+        ),
       ],
     );
   }
--- lib/beam_information/beam_information_display_viewModel.dart
+++ lib/beam_information/beam_information_display_viewModel.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:reflection_simulator/utils/environments_variables.dart';
 
 import '../common/view_model_change_notifier.dart';
+import '../utils/environments_variables.dart';
 import 'beam.dart';
 
 final beamInformationDisplayViewModelProvider =
@@ -12,20 +13,28 @@ final beamInformationDisplayViewModelProvider =
 class BeamInformationDisplayViewModel extends ViewModelChangeNotifier {
   BeamInformationDisplayViewModel() {
     currentBeam = example_beam;
+    beamWaistInputController =
+        TextEditingController(text: currentBeam.beamWaist.toString());
+    beamWaveLengthInputController =
+        TextEditingController(text: currentBeam.waveLength.toString());
   }
 
   late Beam currentBeam;
+  late TextEditingController beamWaveLengthInputController;
+  late TextEditingController beamWaistInputController;
 
   void changeBeamType(String newValue) {
     currentBeam.type = newValue;
     notifyListeners();
   }
 
-  void changeBeamWaist() {
+  void changeBeamWaist(String newValue) {
+    currentBeam.beamWaist = double.parse(newValue);
     notifyListeners();
   }
 
-  void changeWaveLength() {
+  void changeWaveLength(String newValue) {
+    currentBeam.waveLength = double.parse(newValue);
     notifyListeners();
   }
 }
