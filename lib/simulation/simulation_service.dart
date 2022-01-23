6bbf62738e0bd94f250ca105cc18712ac9dad7fa
小田哲秀 <odanorihide@odatesshuunoMacBook-Pro.local>
2022-01-23 18:17:15 +0900

    simulationServiceの作成

--- lib/simulation/simulation_service.dart
+++ lib/simulation/simulation_service.dart
@@ -2,42 +2,89 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vector_math/vector_math.dart';
 
 import '../beam_information/beam.dart';
-import '../common/view_model_change_notifier.dart';
 import '../optics_diagram/optics.dart';
-import '../utils/environments_variables.dart';
+import '../utils/graph.dart';
 
-final simulationServiceProvider = ChangeNotifierProvider.autoDispose(
+class SimulationResult {
+  SimulationResult(
+      this.reflectionPositions, this.simulatedBeamList);
+  final List<List<Map<int, Vector3>>> reflectionPositions;
+  final List<Beam> simulatedBeamList;
+}
+
+
+final simulationServiceProvider = Provider(
   (ref) => SimulationService(),
 );
 
-class SimulationService extends ViewModelChangeNotifier {
-  SimulationService() {
-    currentOpticsList = initialOpticsList;
-    currentBeam = initialBeam;
-    simulatedReflectPositions = runSimulation();
-  }
-
-  late List<Optics> currentOpticsList;
-  late Beam currentBeam;
-  late List<Vector3> simulatedReflectPositions;
-  
-
-
-  List<Vector3> runSimulation() {
-    // インスタンスをコピー
-    final simulationBeam = Beam(
-      type: currentBeam.type,
-      waveLength: currentBeam.waveLength,
-      beamWaist: currentBeam.beamWaist,
-      startFrom: currentBeam.startFrom,
+class SimulationService {
+  SimulationResult runSimulation({
+    required Beam currentBeam,
+    required Graph<Optics> currentOpticsTree,
+    required List<Optics> currentOpticsList,
+  }) {
+    // 初期化
+    final _seen = List.generate(
+      currentOpticsTree.nodes.keys.length,
+      (index) => false,
     );
-    final reflectPositions = <Vector3>[currentBeam.startFrom.vector];
+    final reflectionPosition = [
+      [
+        {-1: currentBeam.startFrom.vector}
+      ],
+    ];
 
-    for (var i = 0; i < currentOpticsList.length; i++) {
-      reflectPositions.add(simulationBeam.reflect(currentOpticsList[i]));
+    var branchID = 0;
+    final _simulatedBeamList = [
+      Beam(
+        type: currentBeam.type,
+        waveLength: currentBeam.waveLength,
+        beamWaist: currentBeam.beamWaist,
+        startFrom: currentBeam.startFrom,
+      ),
+    ];
+
+    // 深さ優先探索
+    void _dfs(Graph<Optics> g, Node<Optics> v) {
+      _seen[v.id] = true;
+      final nodes = g.nodes[v];
+
+      // 枝分かれを検出した時
+      if (nodes!.length > 1) {
+        for (var i = 0; i < nodes.length - 1; i++) {
+          final tmpBeam = Beam(
+            type: _simulatedBeamList[branchID].type,
+            waveLength: _simulatedBeamList[branchID].waveLength,
+            beamWaist: _simulatedBeamList[branchID].beamWaist,
+            startFrom: _simulatedBeamList[branchID].startFrom,
+          )..reachTo(v.data);
+          _simulatedBeamList.add(tmpBeam);
+
+          reflectionPosition.add([
+            {v.id: tmpBeam.startPositionVector}
+          ]);
+        }
+      }
+
+      final position = _simulatedBeamList[branchID].reflect(v.data);
+      reflectionPosition[branchID].add({v.id: position});
+
+      for (final nextV in nodes) {
+        if (_seen[nextV.id]) {
+          continue;
+        }
+        _dfs(g, nextV);
+      }
+      // 末端に到達した数 = 枝の数
+      if (nodes.isEmpty) {
+        branchID++;
+      }
     }
-    simulatedReflectPositions = reflectPositions;
-    notifyListeners();
-    return reflectPositions;
+
+    _dfs(
+      currentOpticsTree,
+      currentOpticsTree.nodes.keys.first,
+    );
+    return SimulationResult(reflectionPosition,_simulatedBeamList);
   }
 }
