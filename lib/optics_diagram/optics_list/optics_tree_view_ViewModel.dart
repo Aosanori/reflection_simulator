16383efc3eaa3167bc3461ad5468ea04aa729ec3
小田哲秀 <odanorihide@odatesshuunoMacBook-Pro.local>
2022-01-23 18:56:58 +0900

    その他

--- lib/optics_diagram/optics_list/optics_tree_view_ViewModel.dart
+++ lib/optics_diagram/optics_list/optics_tree_view_ViewModel.dart
@@ -1,37 +1,40 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import '../../common/view_model_change_notifier.dart';
-import '../../simulation/simulation_service.dart';
 
+import '../../common/view_model_change_notifier.dart';
+import '../../simulation/optics_state.dart';
+import '../../simulation/simulation_state_store_service.dart';
 import '../optics.dart';
 
 final opticsListViewViewModelProvider = ChangeNotifierProvider.autoDispose(
-  (ref) => OpticsListViewViewModel(ref.watch(simulationServiceProvider)),
+  (ref) => OpticsListViewViewModel(
+      ref.watch(simulationStateStoreProvider), ref.watch(opticsStateProvider)),
 );
 
 class OpticsListViewViewModel extends ViewModelChangeNotifier {
-  OpticsListViewViewModel(this._simulationService);
-  final SimulationService _simulationService;
-  List<Optics> get currentOpticsList => _simulationService.currentOpticsList;
+  OpticsListViewViewModel(this._simulationStateStore, this._opticsState);
+  final SimulationStateStore _simulationStateStore;
+  final OpticsState _opticsState;
+  List<Optics> get currentOpticsList =>
+      _simulationStateStore.state.currentOpticsList;
 
   void dragAndDrop(int oldIndex, int newIndex) {
-    if (newIndex > oldIndex) {
+    /*if (newIndex > oldIndex) {
       // 元々下にあった要素が上にずれるため一つ分後退させる
       // ignore: parameter_assignments
       newIndex -= 1;
     }
 
     // 並び替え処理
-    final optics = _simulationService.currentOpticsList[oldIndex];
-    _simulationService.currentOpticsList
+    final optics = _simulationStateStore.state.currentOpticsList[oldIndex];
+    _simulationStateStore.state.currentOpticsList
       ..removeAt(oldIndex)
       ..insert(newIndex, optics);
     notifyListeners();
-    _simulationService.runSimulation();
+    //_simulationStateStore.runSimulation();*/
   }
 
   void removeContent(int index) {
-    _simulationService.currentOpticsList.removeAt(index);
+    _opticsState.deleteOptics(index);
     notifyListeners();
-    _simulationService.runSimulation();
   }
 }
