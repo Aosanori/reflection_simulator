4021e9da25dda3d08cdacb71552835b7707bd812
小田哲秀 <odanorihide@odatesshuunoMacBook-Pro.local>
2022-01-27 18:54:31 +0900

    createRelation

--- lib/optics_diagram/optics_list/optics_list_view.dart
+++ lib/optics_diagram/optics_list/optics_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../create_optics/create_optics_dialog.dart';
 import '../optics_diagram_item/optics_diagram_item.dart';
 
 import 'optics_tree_view_ViewModel.dart';
@@ -11,24 +12,34 @@ class OpticsListView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.watch(opticsListViewViewModelProvider);
     final currentOpticsList = viewModel.currentOpticsList;
-    return ReorderableListView(
-      onReorder: viewModel.dragAndDrop,
-      buildDefaultDragHandles: false,
-      padding: const EdgeInsets.only(top: 4),
-      children: currentOpticsList
-          .asMap()
-          .map(
-            (index, item) => MapEntry(
-              index,
-              OpticsDiagramItem(
-                index: index,
-                onDelete: () => viewModel.removeContent(index),
-                key: Key(item.id),
+    return Scaffold(
+      body: ReorderableListView(
+        onReorder: viewModel.dragAndDrop,
+        buildDefaultDragHandles: false,
+        padding: const EdgeInsets.only(top: 4),
+        children: currentOpticsList
+            .asMap()
+            .map(
+              (index, item) => MapEntry(
+                index,
+                OpticsDiagramItem(
+                  index: index,
+                  onDelete: () => viewModel.removeContent(index),
+                  key: Key(item.id),
+                ),
               ),
-            ),
-          )
-          .values
-          .toList(),
+            )
+            .values
+            .toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showDialog<CreateOpticsDialog>(
+          context: context,
+          builder: (_) => CreateOpticsDialog(),
+        ),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
--- lib/optics_diagram/optics_tree/optics_tree_view.dart
+++ lib/optics_diagram/optics_tree/optics_tree_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:reflection_simulator/create_optics/create_optics_relation_dialog.dart';
+import '../../create_optics/create_optics_relation_dialog.dart';
 
 class OpticsTreeView extends HookConsumerWidget {
   const OpticsTreeView({Key? key}) : super(key: key);
